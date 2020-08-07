ed70593c899d4c07acc89f1b11395f788f21cb89
David Allen <davidallen02@me.com>
2020-08-07 14:42:11 -0700

    Add new chart

---
+++ R/spending-level-by-type-normalized.R
@@ -0,0 +1,15 @@
+library(magrittr)
+
+dat <- pamngr::join_sheets("pce cur$") %>%
+  pamngr::normalize("2019-12-31") %>%
+  reshape2::melt(id.vars = "dates")
+
+p <- dat %>%
+  pamngr::lineplot() %>%
+  pamngr::pam_plot(
+    plot_title = "Personal Consumption Expenditures",
+    plot_subtitle = "Normalized to December 2019",
+    show_legend = FALSE
+  ) 
+
+p %>% pamngr::all_output("spending-level-normalized")
