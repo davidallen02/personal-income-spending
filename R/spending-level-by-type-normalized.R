b49e8ca83c420f33c2577af0efb000d68f3ed34b
davidallen02 <davidallen02@me.com>
2020-08-07 14:49:51 -0700

    updates chart

--- R/spending-level-by-type-normalized.R
+++ R/spending-level-by-type-normalized.R
@@ -1,15 +1,15 @@
 library(magrittr)
 
-dat <- pamngr::join_sheets("pce cur$") %>%
+dat <- pamngr::join_sheets(c("uspxgood", "pce srv")) %>%
   pamngr::normalize("2019-12-31") %>%
+  magrittr::set_colnames(c("dates","Goods","Services")) %>%
   reshape2::melt(id.vars = "dates")
 
 p <- dat %>%
   pamngr::lineplot() %>%
   pamngr::pam_plot(
     plot_title = "Personal Consumption Expenditures",
-    plot_subtitle = "Normalized to December 2019",
-    show_legend = FALSE
+    plot_subtitle = "Normalized to December 2019"
   ) 
 
-p %>% pamngr::all_output("spending-level-normalized")
+p %>% pamngr::all_output("spending-level-by-type-normalized")
