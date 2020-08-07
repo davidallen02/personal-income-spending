0befbeedff4297863cddf134c4a7185ab18d8f45
davidallen02 <davidallen02@me.com>
2020-08-07 13:13:27 -0700

    new plot

--- R/spending-level-normalized.R
+++ R/spending-level-normalized.R
@@ -1,13 +1,15 @@
 library(magrittr)
 
 dat <- pamngr::get_data("pce cur$") %>%
-  dplyr::filter(dates >= as.POSIXct("2020-01-01")) %>%
+  pamngr::normalize("2019-12-31") %>%
   reshape2::melt(id.vars = "dates")
 
 p <- dat %>%
   pamngr::lineplot() %>%
   pamngr::pam_plot(
     plot_title = "Personal Consumption Expenditures",
-    plot_subtitle = "Billions of USD",
+    plot_subtitle = "Normalized to December 2019",
     show_legend = FALSE
   ) 
+
+p %>% pamngr::all_output("spending-level-normalized")
