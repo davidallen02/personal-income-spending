37e8ccf4df065470bf83e0777017e582d5f0e66a
David Allen <davidallen02@me.com>
2020-08-07 14:21:30 -0700

    Adds percent change in spending by category

--- R/spending-pchange-category.R
+++ R/spending-pchange-category.R
@@ -1,2 +1,63 @@
-pamngr::join_sheets(c("pce cur$", "pidss"))
-pamngr::get_data("pce cur$")
+goods <- pamngr::join_sheets(c("uspxmvpa",
+                               "uspxfadh",
+                               "uspxrgav",
+                               "uspxodrg",
+                               "uspxfbof",
+                               "uspxclaf",
+                               "uspxgaog",
+                               "uspxondg",
+                               "uspxnxus")) %>%
+  magrittr::set_colnames(c(
+    "dates",
+    "Motor Vehicles and Parts",
+    "Furnishings and Durable Household Equipment",
+    "Recreational Goods and Vehicles",
+    "Other Durable Goods",
+    "Food and Beverage for Off-Premises Consumption",
+    "Clothing and Footwear",
+    "Gasoline and Other Energy Goods",
+    "Other Nondurable Goods",
+    "Net Personal Consumption Abroad"
+  )) %>%
+  dplyr::mutate(type = "Goods") %>%
+  reshape2::melt(id.vars = c("dates", "type"))
+
+services <- pamngr::join_sheets(c("uspxhaut",
+                                  "uspxhelc",
+                                  "uspxtrso",
+                                  "uspxrcrs",
+                                  "uspxfoae",
+                                  "uspxfsvi",
+                                  "uspxosvc")) %>%
+  magrittr::set_colnames(c(
+    "dates",
+    "Housing and Utilities",
+    "Health Care",
+    "Transportation Services",
+    "Recreation Services",
+    "Food Service and Accomodations",
+    "Financial Services and Insurance",
+    "Other Services"
+  )) %>%
+  dplyr::mutate(type = "Services") %>%
+  reshape2::melt(id.vars = c("dates","type")) 
+
+dat <- goods %>%
+  dplyr::bind_rows(services) %>%
+  pamngr::pchange(k = 7) %>%
+  dplyr::slice_max(dates, n =1) %>%
+  dplyr::arrange(value) %>%
+  dplyr::mutate(
+    variable = variable %>% factor(levels = variable)
+  )
+
+p <- dat %>%
+  pamngr::barplot(x = "variable", y = "value", fill = "type") %>%
+  pamngr::pam_plot(
+    plot_title = "Personal Spending",
+    plot_subtitle = "Percent Change Since December 2019"
+  )
+
+p <- p + ggplot2::coord_flip()
+
+p %>% pamngr::all_output("spending-pchange-category")
