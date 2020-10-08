f876e4402230aaf0c569137b070c81df00750dab
David Allen <davidallen02@me.com>
2020-10-08 15:19:38 -0700

    update charts

--- R/personal-savings.R
+++ R/personal-savings.R
@@ -1,12 +1,17 @@
-pamngr::get_data("pidss") %>%
+dat <-pamngr::join_sheets(c("pidss", "pidsdi")) %>%
+  dplyr::mutate(savings_rate = pidss/pidsdi * 100) %>%
+  dplyr::select(dates, savings_rate) %>%
   reshape2::melt(id.vars = "dates") %>%
-  dplyr::filter(dates >= as.POSIXct("2017-01-01")) %>%
+  dplyr::filter(dates >= as.POSIXct("2017-01-01"))
+
+p <- dat %>%
   pamngr::lineplot() %>%
   pamngr::pam_plot(
     plot_title = "Personal Savings Rate",
-    plot_subtitle = "Percent of Personal Income",
+    plot_subtitle = "Percent of Disposable Personal Income",
     show_legend = FALSE
-  ) %>%
-  pamngr::all_output("personal-savings")
+  )
+
+p %>% pamngr::all_output("personal-savings")
 
 
